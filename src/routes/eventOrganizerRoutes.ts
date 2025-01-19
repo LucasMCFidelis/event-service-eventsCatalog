34e519fc00755173d8fa824adb12cdfc786097a0
Lucas Fidelis <lucasm241301@gmail.com>
2025-01-19 20:06:48 -0300

    Adiciona autenticação e checagem do tipo de permissão nas rotas de eventos e organizadores

--- src/routes/eventOrganizerRoutes.ts
+++ src/routes/eventOrganizerRoutes.ts
@@ -1,10 +1,31 @@
 import { FastifyInstance } from "fastify";
-import { createEventOrganizerRoute, deleteEventOrganizerRoute, getEventOrganizerByIdRoute, listEventOrganizerRoute, updateEventOrganizerRoute } from "../controllers/eventOrganizerController.js";
+import {
+  createEventOrganizerRoute,
+  deleteEventOrganizerRoute,
+  getEventOrganizerByIdRoute,
+  listEventOrganizerRoute,
+  updateEventOrganizerRoute,
+} from "../controllers/eventOrganizerController.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { checkRole } from "../middlewares/checkRole.js";
+import { EventOrganizer } from "@prisma/client";
 
-export async function eventOrganizerRoutes(server:FastifyInstance) {
-    server.post("/", createEventOrganizerRoute)
-    server.get("/", listEventOrganizerRoute)
-    server.get("/:id", getEventOrganizerByIdRoute)
-    server.delete("/:id", deleteEventOrganizerRoute)
-    server.put("/:id", updateEventOrganizerRoute)
+export async function eventOrganizerRoutes(server: FastifyInstance) {
+  server.post<{ Body: EventOrganizer }>(
+    "/",
+    { preHandler: [authMiddleware, checkRole("Admin")] },
+    createEventOrganizerRoute
+  );
+  server.get("/", listEventOrganizerRoute);
+  server.get("/:id", getEventOrganizerByIdRoute);
+  server.delete<{ Params: { id: string } }>(
+    "/:id",
+    { preHandler: [authMiddleware, checkRole("Admin")] },
+    deleteEventOrganizerRoute
+  );
+  server.put<{ Params: { id: string }; Body: Partial<EventOrganizer> }>(
+    "/:id",
+    { preHandler: [authMiddleware, checkRole("Admin")] },
+    updateEventOrganizerRoute
+  );
 }
--- src/routes/eventRoutes.ts
+++ src/routes/eventRoutes.ts
@@ -6,11 +6,26 @@ import {
   listEventsRoute,
   updateEventRoute,
 } from "../controllers/eventController.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { checkRole } from "../middlewares/checkRole.js";
+import { Event } from "@prisma/client";
 
 export async function eventRoutes(server: FastifyInstance) {
-  server.post("/", createEventRoute); // POST /events
+  server.post<{ Body: Event }>(
+    "/",
+    { preHandler: [authMiddleware, checkRole("Admin")] },
+    createEventRoute
+  ); // POST /events
   server.get("/", listEventsRoute); // GET /events
   server.get("/:id", getEventByIdRoute); // GET /events/:id
-  server.delete("/:id", deleteEventRoute); // DELETE /events/:id
-  server.put("/:id", updateEventRoute); // UPDATE /events/:id
+  server.delete<{ Params: { id: string } }>(
+    "/:id",
+    { preHandler: [authMiddleware, checkRole("Admin")] },
+    deleteEventRoute
+  ); // DELETE /events/:id
+  server.put<{ Params: { id: string }; Body: Partial<Event> }>(
+    "/:id",
+    { preHandler: [authMiddleware, checkRole("Admin")] },
+    updateEventRoute
+  ); // UPDATE /events/:id
 }
